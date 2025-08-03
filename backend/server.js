d1e12d7c9b5e65abcd9efc9c205d653e32ea5773
imrozkhan205 <imrozkhan2258@gmail.com>
2025-08-03 23:58:45 +0530

    Removed unnecessary if loop in server.js

--- backend/server.js
+++ backend/server.js
@@ -50,12 +50,12 @@ app.use('/api/auth', authRoutes);
 app.use('/api/tickets', ticketRoutes);
 app.use('/api/users', userRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
 
 app.get('/', (req, res) => {
     res.send('Server is running with Socket.IO support!');
