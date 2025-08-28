2012a9ce59ca5082928c0dca483fd01cf6b5095d
imrozkhan205 <imrozkhan2258@gmail.com>
2025-08-28 15:09:20 +0530

    Changed the VITE_API_URL

--- frontend/src/components/pages/ChatPage.jsx
+++ frontend/src/components/pages/ChatPage.jsx
@@ -14,7 +14,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import { axiosInstance } from "../../lib/axios";
 import io from "socket.io-client";
 
-const SOCKET_SERVER_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:5000";
+const SOCKET_SERVER_URL = import.meta.env.VITE_SOCKET_URL || "https://sts-a6ed.onrender.com/";
 
 
 const ChatPage = ({ user }) => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_URI,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
